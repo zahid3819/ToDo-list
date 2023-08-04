4d9adba9c80274fbbbe6322cd9b715e6d8f320bf
Zahid Hussain <zahid.hussain@zonicsolutions.com>
2023-08-04 18:45:26 +0500

    Todo App starter

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.flexForTodo}>
+        <TextInput placeholder='Add your Todo'  style={styles.textInput}/>
+        <Button title='Add Todo' style={styles.buttonToDo}/>
+      </View>
+      <View>
+        <Text>
+          All Todos Here!
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   paddingTop: 30,
+   paddingHorizontal: 10
   },
+  flexForTodo:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: "80%",
+    marginRight: 5,
+    padding: 8
+
+  },
+  buttonToDo:{
+    borderRadius: 10,
+  }
+  
 });
