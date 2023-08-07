6476ffaaa88277659d65f2bb217243a65b117643
Zahid Hussain <zahid.hussain@zonicsolutions.com>
2023-08-07 11:50:11 +0500

    add Todo and Show Todo

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+  //To store Todo 
+  const [enteredTodoText, setEnteredTodoText] = useState('');
+  //to show Todo
+  const [showTodosList, setShowTodosList] = useState([]);
+
+  function enterTodoText(enteredText){
+    setEnteredTodoText(enteredText);
+  }
+
+  function addTodoText(){
+    if(enteredTodoText != ""){
+    setShowTodosList([...showTodosList, enteredTodoText]);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.flexForTodo}>
-        <TextInput placeholder='Add your Todo'  style={styles.textInput}/>
-        <Button title='Add Todo' style={styles.buttonToDo}/>
+        <TextInput placeholder='Add your Todo' onChangeText={enterTodoText} style={styles.textInput}/>
+        <Button title='Add Todo' onPress={addTodoText} style={styles.buttonToDo}/>
       </View>
-      <View>
-        <Text>
-          All Todos Here!
-        </Text>
-      </View>
+      <ScrollView>
+        {showTodosList.map((todo) => <Text style={styles.todo} key={todo}>{todo}</Text>)}
+      </ScrollView>
     </View>
   );
 }
@@ -24,6 +38,11 @@ const styles = StyleSheet.create({
   flexForTodo:{
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    paddingBottom: 20,
+    marginBottom: 20,
   },
   textInput:{
     borderWidth: 1,
@@ -35,6 +54,14 @@ const styles = StyleSheet.create({
   },
   buttonToDo:{
     borderRadius: 10,
+  },
+  todo:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    marginVertical: 5,
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 24,
   }
   
 });
